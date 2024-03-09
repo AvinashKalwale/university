3c6c38b9d590f935bbddc3817992041bdc0462a3
Avinash Kalwale <avinashkalwale12@gmail.com>
2024-03-09 22:17:38 +0530

    click by side video back to the site

--- educationSity/src/components/VideoPlayer/VideoPlayer.jsx
+++ educationSity/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React , {useRef} from 'react'
 import './VideoPlayer.css'
 import college_video from '../../assets/college-video.mp4'
 
 const VideoPlayer = ({playVideo, setPlayVideo}) => {
+
+const player=useRef(null);
+const closePlayer=(e)=>{
+    if(e.target===player.current){
+        setPlayVideo(false)
+    }
+}
   return (
-    <div className={`videoPlayer ${playVideo ? "":"hide"}`}>
+    <div className={`videoPlayer ${playVideo ? "":"hide"}`} ref={player}
+    onClick={closePlayer}>
     <video src={college_video} autoPlay muted controls></video></div>
   )
 }
